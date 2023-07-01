f9cd6ef7a4e179382c1570f065d0ee006d897f4e
emilyyguo <137805028+emilyyguo@users.noreply.github.com>
2023-07-01 14:19:19 -0500

    Update graphics.cpp
    comments finished, need review

--- graphics.cpp
+++ graphics.cpp
@@ -1,7 +1,27 @@
-#include "graphics.h"
+/*
+* Author: Logan, Tanner, Emily
+* Assignment Title: Group Project - Asteroids
+* Assignment Description:
+*     - This is the graphics.cpp file which contains the graphics member
+*       function definitions and these functions determine the graphics
+*       of the rocket and the other elements of the game.
+* Due Date: 7/7/2023
+* Date Created: 6/30/2023
+* Date Last Modified: 7/1/2023
+*/
 
+#include "graphics.h"
 #include "point.h"
 
+/*
+* description: Draws the lines of the rocket by plotting the points that shape
+*              the rocket, by first determining if there the point coordinates
+*              have a slope and then the lines are drawn according to the slope
+*              of the distance betewen the first and second points
+* return: None
+* precondition: x1, y1, x2, and y2 are valid
+* postcondition: The lines of the rocket will be drawn
+*/
 void drawLine(int x1, int y1, int x2, int y2, color c, SDL_Plotter& g){
     double m, b, y;
     if(hasSlope(x1,y1,x2,y2)){
@@ -19,6 +39,12 @@ void drawLine(int x1, int y1, int x2, int y2, color c, SDL_Plotter& g){
     }
 }
 
+/*
+* description: The function draws the shapes for the graphics on the screen
+* return: None
+* precondition: point location, shape vector, color and graphics are valid
+* postcondition: The shape is drawn
+*/
 void drawPoly(const point_t& loc, const vector<point_t>& shape, color c, SDL_Plotter& g) {
     for (size_t i = 0; i < shape.size(); i++) {
         int x1 = static_cast<int>(loc.getX() + shape[i].getX());
@@ -30,6 +56,13 @@ void drawPoly(const point_t& loc, const vector<point_t>& shape, color c, SDL_Plo
     }
 }
 
+/*
+* description: The function moves the shape based on the x and y values of the
+*              shape's points
+* return: None
+* precondition: vector x, vector y, and the x-value for the point is valid
+* postcondition: Moves the shape
+*/
 void movePoly(vector<int>& x, vector<int>& y, int px){
     for(int i=0; i < x.size(); i++){
         x[i] += px;
@@ -37,6 +70,13 @@ void movePoly(vector<int>& x, vector<int>& y, int px){
     }
 }
 
+/*
+* description: The functions draws the bullet shape which is a circular shape,
+*              using the location, size, and color to plot the circle
+* return: None
+* precondition: loc, size, c, and g are valid
+* postcondition: Plots a circular shape
+*/
 void drawCircle(point loc, int size, color c, SDL_Plotter& g){
 	for(double i = -size; i <= size;i+=0.1){
 		for(double j = -size; j <= size; j+=0.1){
@@ -48,14 +88,34 @@ void drawCircle(point loc, int size, color c, SDL_Plotter& g){
 
 }
 
+/*
+* description: Determines if a line has slope
+* return: x1 != x2;
+* precondition: x1, y1, x2, and y2 are valid
+* postcondition: The slope exists if the x values of two points are not equal
+*/
 bool hasSlope(int x1, int y1, int x2, int y2){
     return x1 != x2;
 }
 
+/*
+* description: Calculates the slope of the point based on the x and y
+*              coordinates of two points
+* return: slope
+* precondition: x1, y1, x2, and y2 are valid
+* postcondition: Returns the slope
+*/
 double slope(int x1, int y1, int x2, int y2){
     return static_cast<double>(y1 - y2)/(x1 - x2);
 }
 
+/*
+* description: Calculates the y-intercerpt of the point based on the first y
+*              coordinate and the slope of the point.
+* return: y-intercept
+* precondition: x1, y1, x2, and y2 are valid
+* postcondition: Returns y-intercept
+*/
 double yIntercept(int x1, int y1, int x2, int y2){
     return y1 - slope(x1,y1,x2,y2) *  x1;
 }
