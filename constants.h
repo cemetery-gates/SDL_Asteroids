186b12fa12e5d153c76ab77694f22e9cbc8b2b9f
cemetery-gates <49575599+cemetery-gates@users.noreply.github.com>
2023-07-01 17:46:21 -0500

    Update constants.h

--- constants.h
+++ constants.h
@@ -1,3 +1,14 @@
+/*
+* Author: Logan, Tanner, Emily
+* Assignment Title: Group Project - Asteroids
+* Assignment Description:
+*     - This is the constants.h file which contains the constant values
+*       for column size (COL), row size (ROW), the background color (BG)
+*       and the Pi (PI)
+* Due Date: 7/7/2023
+* Date Created: 6/30/2023
+* Date Last Modified: 7/1/2023
+*/
 #ifndef CONSTANTS_H_INCLUDED
 #define CONSTANTS_H_INCLUDED
 
