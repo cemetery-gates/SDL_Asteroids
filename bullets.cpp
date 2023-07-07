b4c81000aae9766417b2b680c4600f939706622d
emilyyguo <137805028+emilyyguo@users.noreply.github.com>
2023-07-06 19:33:12 -0500

    Add files via upload
    bullets.h and bullets.cpp
    The rocket.h and rocket.cpp file also has some stuff regarding bullets (needs to be updated). The bullets do shoot from the rocket but the bullets need to be larger and the collision detection is not set up yet. The files will be updated soon.

---
+++ bullets.cpp
@@ -0,0 +1,43 @@
+#include "bullets.h"
+#include "constants.h"
+
+// Takes location, the color, direction,
+Bullet::Bullet(point_t location, double direction) {
+    loc = location;
+    f = force(BULLET_SPEED, direction); //changed if bullets are going to fast/slow
+    c = color(255, 0, 0);
+    offScreen = false;
+    hasCollided = false;
+}
+
+void Bullet::draw(SDL_Plotter& g) {
+    g.plotPixel(loc.getX(), loc.getY(), c.R, c.G, c.B);
+}
+
+void Bullet::erase(SDL_Plotter& g) {
+    g.plotPixel(loc.getX(), loc.getY(), BG.R, BG.G, BG.B);
+}
+
+void Bullet::move() {
+    double newX = loc.getX() + f.getMagnitude() * cos(f.getDirection());
+    double newY = loc.getY() + f.getMagnitude() * sin(f.getDirection());
+
+
+    if (newX < 0 || newX > COL || newY < 0 || newY > ROW) {
+        offScreen = true;
+        return;
+    }
+
+    //FINISH COLLISION
+    //if hit asteroid, set hasCollided to true, and have return statement
+    loc.setX(newX);
+    loc.setY(newY);
+}
+
+bool Bullet::isOffScreen() const {
+    return offScreen;
+}
+
+bool Bullet::isColliding() const {
+    return hasCollided;
+}
---
+++ bullets.h
@@ -0,0 +1,28 @@
+#ifndef BULLETS_H_INCLUDED
+#define BULLETS_H_INCLUDED
+
+#include "point.h"
+#include "SDL_Plotter.h"
+#include "constants.h"
+#include "graphics.h"
+#include "rocket.h"
+
+class Bullet {
+    private:
+        point_t loc;
+        force f;
+        color c;
+        bool offScreen;
+        bool hasCollided;
+
+    public:
+        Bullet(point_t location, double direction);
+        void draw(SDL_Plotter& g);
+        void erase(SDL_Plotter& g);
+        void move();
+        bool isOffScreen() const;
+        bool isColliding() const;
+};
+
+
+#endif // BULLETS_H_INCLUDED
